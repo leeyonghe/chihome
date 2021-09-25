b7a4dc060675dd022cf9fa735f671d29e90c87ab
leeyonghe <lyhdra99@gmail.com>
2021-09-25 19:54:22 +0900

    init

--- lib/loginpage.dart
+++ lib/loginpage.dart
@@ -1,6 +1,20 @@
 import 'package:flutter/material.dart';
 import 'mainpage.dart';
 
+import 'dart:async';
+import 'dart:convert' show json;
+import "package:http/http.dart" as http;
+import 'package:google_sign_in/google_sign_in.dart';
+
+GoogleSignIn _googleSignIn = GoogleSignIn(
+  // Optional clientId
+  // clientId: '479882132969-9i9aqik3jfjd7qhci1nqf0bm2g71rm1u.apps.googleusercontent.com',
+  scopes: <String>[
+    'email',
+    'https://www.googleapis.com/auth/contacts.readonly',
+  ],
+);
+
 class LoginPage extends StatefulWidget {
 
   const LoginPage({Key? key}) : super(key: key);
@@ -14,7 +28,42 @@ class LoginPage extends StatefulWidget {
 
 class LoginState extends State<LoginPage> {
   
-  
+  GoogleSignInAccount? _currentUser;
+
+  @override
+  void initState() {
+    super.initState();
+
+    _googleSignIn.onCurrentUserChanged.listen((GoogleSignInAccount? account) {
+      setState(() {
+        _currentUser = account;
+        Navigator.push(
+          context,
+          MaterialPageRoute(builder: (context) => const MainPage()),
+        );
+      });
+      if (_currentUser != null) {
+        _handleGetContact(_currentUser!);
+      }
+    });
+
+  }
+
+  Future<void> _handleGetContact(GoogleSignInAccount user) async {
+    final http.Response response = await http.get(
+      Uri.parse('https://people.googleapis.com/v1/people/me/connections'
+          '?requestMask.includeField=person.names'),
+      headers: await user.authHeaders,
+    );
+    if (response.statusCode != 200) {
+       Navigator.push(
+        context,
+        MaterialPageRoute(builder: (context) => const MainPage()),
+      );
+      return;
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(home: Scaffold(
@@ -31,6 +80,7 @@ class LoginState extends State<LoginPage> {
                           // stretch across width of screen
                           children: <Widget>[
                             const SizedBox(child: Image(image: AssetImage('images/logo.jpg')), width: 200, height: 200,),
+                            _buildGoogleLoginButton(),
                             _buildKakaoLoginButton(),
                             _buildNaverLoginButton(),
                             _buildAppleLoginButton(),
@@ -42,6 +92,25 @@ class LoginState extends State<LoginPage> {
           )),
     ));
   }
+
+
+
+Container _buildGoogleLoginButton() {
+  final ButtonStyle style = ElevatedButton.styleFrom(textStyle: const TextStyle(fontSize: 15));
+    return Container(
+      margin: const EdgeInsets.only(left: 16, top: 16, right: 16, bottom: 0),
+      child: ElevatedButton(
+           style: style,
+           onPressed: () => {
+              // Navigator.push(
+              //   context,
+              //   MaterialPageRoute(builder: (context) => const MainPage()),
+              // )
+              _googleSignIn.signInSilently()
+           },
+           child: const Text('Apple'),
+      ));
+}
 
 Container _buildAppleLoginButton() {
   final ButtonStyle style = ElevatedButton.styleFrom(textStyle: const TextStyle(fontSize: 15));
